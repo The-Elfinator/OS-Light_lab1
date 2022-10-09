4e1b97e7d0846fb6e63ee2d7d5f52fb538bce9bf
The-Elfinator <49344944+The-Elfinator@users.noreply.github.com>
2022-10-09 19:13:29 +0300

    Delete taskVI.sh

--- taskVI.sh
+++
@@ -1,6 +0,0 @@
-#! ../../../bin/bash
-
-awk '{if ($3 == "(WW)") print}' ../../../var/log/anaconda/X.log > full.log
-awk '{if ($3 == "(II)") print}' ../../../var/log/anaconda/X.log >> full.log
-sed -i -e 's/(WW)/Warning:/g' full.log
-sed -i -e 's/(II)/Information:/g' full.log
