229b0e2d2db0bddde04c840c5c2dfaf60c0e6d4e
The-Elfinator <49344944+The-Elfinator@users.noreply.github.com>
2022-12-17 12:09:57 +0300

    Uploaded mod 3lab
    File parser.sh

--- lab3/parser.sh
+++ lab3/parser.sh
@@ -24,16 +24,36 @@ res=1
 			"*")
 				mode="MUL"
 				;;
+			"-")
+				mode="MINUS"
+				;;
+			"/")
+				mode="DIV"
+				;;
 			*)
-				if ! [[ $LINE =~ ^[0-9]+$ ]]; then
+				if ! [[ $LINE =~ ^[0-9]+(.[0-9]+)?$ ]]; then
 					echo "Error, invalid input parameter! Please enter mode (+ or *), number or QUIT to stop calculating ";
 				else
-					if [[ "$mode" = "PLUS" ]]; then
-						let res=$res+$LINE;
+					if [[ "$mode" == "DIV" && $LINE =~ ^0(.0+)?$ ]]; then
+						echo "Error, you tried to divide by zero!";
 					else
-						let res=$res*$LINE;
+						case "$mode" in
+							"PLUS")
+								res=$(awk "BEGIN {print $res+$LINE}")
+								;;
+							"MUL")
+								res=$(awk "BEGIN {print $res*$LINE}")
+								;;
+							"MINUS")
+								res=$(awk "BEGIN {print $res-$LINE}")
+								;;
+							"DIV")
+								res=$(awk "BEGIN {print $res/$LINE}")
+								#awk 'BEGIN { x='$res'; y='$LINE'; print x, y; r = x/y ;} ; END {}'
+								;;
+						esac
+						echo "Result is: $res"
 					fi
-					echo "Result is: $res"
 				fi
 				;;
 		esac
